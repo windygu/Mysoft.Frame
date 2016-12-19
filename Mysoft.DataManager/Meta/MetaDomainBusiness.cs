b8755cc7601545e4d6780e1a4e7c36f035b3df27
xkcyy <xkcyy@outlook.com>
2016-12-19 22:18:06 +0800

    123

--- Mysoft.DataManager/Meta/MetaDomainBusiness.cs
+++ Mysoft.DataManager/Meta/MetaDomainBusiness.cs
@@ -5,13 +5,27 @@ using System.Text;
 using Mysoft.Core;
 namespace Mysoft.DataManager
 {
-    public class MetaDomainBusiness
+    public class MetaBusiness
     {
         public static List<MetaDomain> GetDomains()
         {
             return DbQuery.GetList<MetaDomain>();
         }
 
+        public static MetaClassDefine GetMetaClassDefine(string classId)
+        {
+            const string sql = @"
+                    select * from MetaClassDefines c where c.Id=@Id;
+                    select * from  MetaPropertyDefine where ClassId=@Id
+                    ";
+            return DbQuery.New().ExecuteList<MetaClassDefine, MetaPropertyDefine>(sql, new { Id = classId }, (x, y) =>
+            {
+                x.ForEach(x1 => x1.PropertyDefineList.AddRange(y.Where(y1 => y1.ClassId == x1.Id)));
+                return x;
+            }).FirstOrDefault();
+        }
         
+
+
     }
 }
